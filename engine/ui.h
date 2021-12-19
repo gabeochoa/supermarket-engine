fc8f042e1cf4b8f1b3c1fcc390e5049c8fb1ca30
gabeochoa <gabeochoa@fb.com>
2021-12-19 18:06:11 -0500

    move typename to top

--- engine/ui.h
+++ engine/ui.h
@@ -1,10 +1,33 @@
 
 #pragma once
 
+#include <string_view>
+
 #include "input.h"
 #include "pch.hpp"
 
 namespace IUI {
+
+template <typename T>
+constexpr auto type_name() {
+    std::string_view name, prefix, suffix;
+#ifdef __clang__
+    name = __PRETTY_FUNCTION__;
+    prefix = "auto type_name() [T = ";
+    suffix = "]";
+#elif defined(__GNUC__)
+    name = __PRETTY_FUNCTION__;
+    prefix = "constexpr auto type_name() [with T = ";
+    suffix = "]";
+#elif defined(_MSC_VER)
+    name = __FUNCSIG__;
+    prefix = "auto __cdecl type_name<";
+    suffix = ">(void)";
+#endif
+    name.remove_prefix(prefix.size());
+    name.remove_suffix(suffix.size());
+    return name;
+}
 
 struct uuid {
     int owner;
@@ -191,29 +214,6 @@ struct WidgetConfig {
 
     WidgetConfig* child;
 };
-
-#include <string_view>
-
-template <typename T>
-constexpr auto type_name() {
-    std::string_view name, prefix, suffix;
-#ifdef __clang__
-    name = __PRETTY_FUNCTION__;
-    prefix = "auto type_name() [T = ";
-    suffix = "]";
-#elif defined(__GNUC__)
-    name = __PRETTY_FUNCTION__;
-    prefix = "constexpr auto type_name() [with T = ";
-    suffix = "]";
-#elif defined(_MSC_VER)
-    name = __FUNCSIG__;
-    prefix = "auto __cdecl type_name<";
-    suffix = ">(void)";
-#endif
-    name.remove_prefix(prefix.size());
-    name.remove_suffix(suffix.size());
-    return name;
-}
 
 template <typename T>
 std::shared_ptr<T> widget_init(uuid id) {
