f3c9dba4b6ff7c30709d57961896a47d2afe320a
gabeochoa <gabeochoa@fb.com>
2022-01-05 01:39:44 -0500

    add contains and add some unit tests

--- engine/vecutil.h
+++ engine/vecutil.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "external_include.h"
+#include "log.h"
 #include "math.h"
 
 inline glm::vec2 lerp(const glm::vec2& x, const glm::vec2& y, float t) {
@@ -55,7 +56,7 @@ inline glm::vec2 round_higher(glm::vec2 val) {
 }
 
 inline glm::vec4 posSizeToRect(glm::vec2 pos, glm::vec2 size) {
-    return glm::vec4{pos, pos.x + size.x, pos.y + size.y};
+    return glm::vec4{pos.x, pos.y, pos.x + size.x, pos.y + size.y};
 }
 
 inline bool aabb(glm::vec4 a, glm::vec4 b) {
@@ -67,8 +68,62 @@ inline bool aabb(glm::vec4 a, glm::vec4 b) {
     );
 }
 
+inline bool contains(glm::vec4 a, glm::vec4 b) {
+    return (           //
+        a.x <= b.x &&  //
+        a.y <= b.y &&  //
+        b.z <= a.z &&  //
+        b.w <= a.w     //
+    );
+}
+
 inline bool aabb(glm::vec2 apos, glm::vec2 asize, glm::vec2 bpos,
                  glm::vec2 bsize) {
     return aabb(posSizeToRect(apos, asize), posSizeToRect(bpos, bsize));
+}
+
+inline void test_no_intersection_no_contains() {
+    auto pos = glm::vec2{0.f, 0.0f};
+    auto size = glm::vec2{1.f, 1.0f};
+    auto rect = posSizeToRect(pos, size);
+
+    auto pos2 = glm::vec2{2.f, 2.0f};
+    auto size2 = glm::vec2{1.f, 1.0f};
+    auto rect2 = posSizeToRect(pos2, size2);
+
+    M_ASSERT(!aabb(rect, rect2), "Rects shouldnt overlap");
+    M_ASSERT(!contains(rect, rect2), "Rects shouldnt overlap");
+}
+
+inline void test_yes_intersection_no_contains() {
+    auto pos = glm::vec2{0.f, 0.0f};
+    auto size = glm::vec2{1.f, 1.0f};
+    auto rect = posSizeToRect(pos, size);
+
+    auto pos2 = glm::vec2{0.5f, 0.5f};
+    auto size2 = glm::vec2{1.f, 1.0f};
+    auto rect2 = posSizeToRect(pos2, size2);
+
+    M_ASSERT(aabb(rect, rect2), "Rects should overlap");
+    M_ASSERT(!contains(rect, rect2), "rect shouldnt contain rect2");
+}
+
+inline void test_yes_intersection_yes_contains() {
+    auto pos = glm::vec2{0.f, 0.0f};
+    auto size = glm::vec2{2.f, 2.0f};
+    auto rect = posSizeToRect(pos, size);
+
+    auto pos2 = glm::vec2{0.5f, 0.5f};
+    auto size2 = glm::vec2{1.f, 1.0f};
+    auto rect2 = posSizeToRect(pos2, size2);
+
+    M_ASSERT(aabb(rect, rect2), "Rects should overlap");
+    M_ASSERT(contains(rect, rect2), "rect should contain rect2");
+}
+
+inline void vectests() {
+    test_no_intersection_no_contains();
+    test_yes_intersection_no_contains();
+    test_yes_intersection_yes_contains();
 }
 
