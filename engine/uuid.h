16346b1f1279be03d6fd00537145c231521d6dee
gabeochoa <gabeochoa@fb.com>
2022-01-09 23:43:40 -0500

    Add index and mk-uuid-loop macro so you can have unique ids in loops :facepalm:

--- engine/uuid.h
+++ engine/uuid.h
@@ -21,7 +21,7 @@ struct uuid {
     int owner;
     std::size_t hash;
 
-    uuid() : uuid(0, "", 0) {}
+    uuid() : uuid(-99, "__MAGIC__STRING__", -1) {}
     uuid(const std::string& s1, int i1) : uuid(-1, s1, i1) {}
 
     uuid(int o, const std::string& s1, int i1) {
@@ -29,6 +29,22 @@ struct uuid {
         auto h1 = std::hash<std::string>{}(s1);
         auto h2 = std::hash<int>{}(i1);
         hash = h1 ^ (h2 << 1);
+    }
+
+    uuid(int o, const std::string& s1, int i1, int index) {
+        owner = o;
+        auto h1 = std::hash<std::string>{}(s1);
+        auto h2 = std::hash<int>{}(i1);
+        auto h3 = std::hash<int>{}(index);
+        hash = h1 ^ (h2 << 1) ^ (h3 << 2);
+    }
+
+    uuid(const uuid& other) { this->operator=(other); }
+
+    uuid& operator=(const uuid& other) {
+        this->owner = other.owner;
+        this->hash = other.hash;
+        return *this;
     }
 
     bool operator==(const uuid& other) const {
@@ -50,6 +66,7 @@ std::ostream& operator<<(std::ostream& os, const uuid& obj) {
 }
 
 #define MK_UUID(x) uuid(x, __FILE__, __LINE__)
+#define MK_UUID_LOOP(x, index) uuid(x, __FILE__, __LINE__, index)
 
 static uuid rootID = MK_UUID(-1);
 static uuid fakeID = MK_UUID(-2);
