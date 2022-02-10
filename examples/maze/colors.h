601dcf4aa21453080ae1d1ddcbba599a4d0c6b98
gabeochoa <gabeochoa@fb.com>
2022-02-09 23:17:24 -0500

    pathfinding test

---
+++ examples/maze/colors.h
@@ -0,0 +1,14 @@
+
+#pragma once
+#include <glm/glm.hpp>
+
+const glm::vec4 BLACK{0.2f};
+const glm::vec4 GRAY{0.5, 0.5f, 0.5f, 1.f};
+const glm::vec4 WHITE{1.f};
+const glm::vec4 CYAN{0.f, 1.f, 1.f, 1.f};
+const glm::vec4 YELLOW{1.f, 1.f, 0.f, 1.f};
+const glm::vec4 PURPLE{0.5f, 0.f, 0.5f, 1.f};
+const glm::vec4 GREEN{0.f, 1.f, 0.f, 1.f};
+const glm::vec4 RED{1.f, 0.f, 0.f, 1.f};
+const glm::vec4 BLUE{0.f, 0.f, 1.f, 1.f};
+const glm::vec4 ORANGE{0.5f, 0.5f, 0.5f, 1.f};
